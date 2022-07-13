4772d9de4aecb92a89f13b70b0192336319e7d6e
HeavenlySea <pavelking11@mail.ru>
2022-07-13 14:01:29 +0400

    Added a path to urls.py home apps

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     # User management
     path("users/", include("butterfly.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
+    path("", include("home.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
