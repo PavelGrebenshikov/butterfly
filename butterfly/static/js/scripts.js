f4a3af0c1dc4909c6bf5f392cf4feac92e22aa66
HeavenlySea <pavelking11@mail.ru>
2022-07-19 11:48:18 +0400

    Added styles for the drop-down menu. Running the script after loading the page.

--- butterfly/static/js/scripts.js
+++ butterfly/static/js/scripts.js
@@ -1,13 +1,18 @@
-let menu = document.querySelector(".menu");
-let dropMenu = document.querySelector(".navigation__dropdown");
+window.onload = function() {
+    let menu = document.querySelector(".menu");
+    let dropMenu = document.querySelector(".navigation__dropdown");
 
-function openDropMenu() {
-    dropMenu.style.display = "flex";
-    dropMenu.style.overflow = "visible";
+    console.log(menu)
+    console.log(dropMenu)
+
+    function openDropMenu() {
+        dropMenu.style.display = "flex";
+        dropMenu.style.overflow = "visible";
+    }
+    function closeDropMenu() {
+        dropMenu.style.display = "none";
+        dropMenu.style.overflow = "hidden";
+    }
+    menu.addEventListener("mouseenter", openDropMenu);
+    dropMenu.addEventListener("mouseleave", closeDropMenu);
 }
-function closeDropMenu() {
-    dropMenu.style.display = "none";
-    dropMenu.style.overflow = "hidden";
-}
-menu.addEventListener("mouseenter", openDropMenu);
-dropMenu.addEventListener("mouseleave", closeDropMenu);
