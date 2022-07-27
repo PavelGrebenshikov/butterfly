a8321efd736024016a06fae7b625a6c0888124e1
Степан Зубков <zubkovbackend@gmail.com>
2022-07-27 17:09:27 +0300

    Add Product to admin, change CategoryAdmin and SubcategoryAdmin

--- butterfly/home/admin.py
+++ butterfly/home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Subcategory
+from .models import Category, Product, Subcategory
 
 
 class SubcategoryInline(admin.StackedInline):
@@ -10,15 +10,35 @@ class SubcategoryInline(admin.StackedInline):
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'subcategories_count']
+    list_display = ['name', 'subcategories_count', 'products_count']
     search_fields = ['name']
     inlines = [SubcategoryInline]
 
     def subcategories_count(self, model: Category) -> int:
         return len(model.subcategory_set.all())
 
+    def products_count(self, model: Category) -> int:
+        return len(model.product_set.all())
+
 
 @admin.register(Subcategory)
 class SubcategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category']
+    list_display = ['name', 'category_name', 'products_count']
     search_fields = ['name']
+
+    def products_count(self, model: Subcategory) -> int:
+        return len(model.product_set.all())
+
+    def category_name(self, model: Subcategory) -> str:
+        return model.category.name
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'categories_names']
+    search_fields = ['name']
+
+    def categories_names(self, model: Product) -> str:
+        categories_set = model.category.all()
+        categories_names = [category.name for category in categories_set]
+        return ', '.join(categories_names)
