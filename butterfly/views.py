d1b388d4c6420574cca63b28364bad53d64397d3
HeavenlySea <pavelking11@mail.ru>
2022-08-08 13:00:54 +0400

    Added a search view. Added templates for product search.

--- butterfly/views.py
+++ butterfly/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchRank
 
 from products.models import Category, Product
 
@@ -9,3 +11,17 @@ def index(request):
         'products': Product.objects.all()
     }
     return render(request, 'home.html', context=context)
+
+def search_product(request):
+    search_vector = SearchVector('name', 'description')
+    query = request.GET.get('search')
+    search_rank = SearchRank(search_vector, query)
+    
+    products = Product.objects.annotate(rank=search_rank).order_by('-rank').values('name', 'rank')
+    print(products)
+    context = {
+        'products': products,
+        'products_count': len(products),
+        'search_product': query,
+    }
+    return render(request, 'products/products_found.html', context=context)
