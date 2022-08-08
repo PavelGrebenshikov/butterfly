4a90e748c76def8a8a6d323445295ac505bf620f
Степан Зубков <zubkovbackend@gmail.com>
2022-08-08 15:31:37 +0300

    Remove unused imports

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,8 @@
-from argparse import Namespace
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 from butterfly.views import index, search_product
 
