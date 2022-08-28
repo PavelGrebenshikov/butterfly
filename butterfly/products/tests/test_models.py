7981ebc94386c0b0661a90d58eb00488c3b02413
HeavenlySea <pavelking11@mail.ru>
2022-08-28 07:57:02 +0400

    Added tests to check the number of created models

--- butterfly/products/tests/test_models.py
+++ butterfly/products/tests/test_models.py
@@ -8,23 +8,31 @@ pytestmark = pytest.mark.django_db
 class TestCategoryModel():
     
     def test_get_absolute_url(self):
+        
+        assert Category.objects.count() == 0
         category = Category.objects.create(name="Category")
         
+        assert Category.objects.count() == 1
         assert category.get_absolute_url() == f"/products/categories/{category.name}"
         
 class TestSubcategoryModel():
     
     def test_get_absolute_url(self):
+        
         category = Category.objects.create(name="Category")
         subcategory = Subcategory.objects.create(name="Subcategory", category=category)
         
+        assert Category.objects.count() and Subcategory.objects.count() == 1
         assert subcategory.get_absolute_url() == f"/products/categories/{subcategory.name}"
 
 class TestProductModel():
     
     def test_get_absolute_url(self):
+        
+        assert Product.objects.count() == 0
         product = Product.objects.create(name = "Test", price = 2333)
         
+        assert Product.objects.count() == 1
         assert product.get_absolute_url() == f"/products/{product.name}"
     
     @pytest.mark.parametrize("image_url, result", [("default.png", "/media/default.png"),
--- butterfly/products/tests/test_views.py
+++ butterfly/products/tests/test_views.py
@@ -1,20 +1,13 @@
-from unicodedata import name
 import pytest
 
 from django.urls import reverse
 
-from butterfly.products.models import Product, Category
+from butterfly.products.models import Category
 
 pytestmark = pytest.mark.django_db
 
 
 class TestProductsView():
-    
-    @pytest.fixture
-    def created_product_objects(self):
-        products = [Product.objects.create(name="first", price=2),
-                    Product.objects.create(name="second", price=3)]
-        return products
     
     def test_view_all_products(self, client):
         
@@ -23,8 +16,8 @@ class TestProductsView():
         
         assert response.status_code == 200
         
-    def test_view_product(self, client, created_product_objects):
-        url = reverse("products:product_page", args={created_product_objects[0].name})
+    def test_view_product(self, client, created_model_objects):
+        url = reverse("products:product_page", args={created_model_objects[0].name})
         response = client.get(url)
         
         assert response.status_code == 200
