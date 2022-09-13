fd8bfdb5e4e1a8699abb7e90edfbdc9ae3a8c915
Pavel <pavelking11@mail.ru>
2022-09-13 13:18:13 +0300

    Added mail subscription model

--- butterfly/contact/models.py
+++ butterfly/contact/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from tabnanny import verbose
+from django.db.models import (
+    Model, EmailField, DateTimeField
+)
 
-# Create your models here.
+class Subscription(Model):
+    # Mail newsletter subscription
+
+    email = EmailField(_("Email"))
+    date = DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.email
+
+    class Meta:
+        app_label = "contact"
+        verbose_name = _("Email")
+        verbose_name_plural = _("Emails")
