615edd63524066f6d4495e40c91c4aece66a5aa8
Pavel <pavgrebens@mail.ru>
2022-10-10 16:08:49 +0300

    Removed dbmail application, added Redis and Celery settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,7 +70,6 @@ DJANGO_APPS = [
     # "django.contrib.humanize", # Handy template tags
     "django.contrib.admin",
     "django.forms",
-    "dbmail",
 ]
 THIRD_PARTY_APPS = [
     "crispy_forms",
@@ -289,10 +288,14 @@ SOCIALACCOUNT_FORMS = {"signup": "butterfly.users.forms.UserSocialSignupForm"}
 
 # Your stuff...
 # ------------------------------------------------------------------------------
-DB_MAILER_ALLOWED_MODELS_ON_ADMIN = [
-    'MailFromEmailCredential',
-    'MailFromEmail',
-    'MailTemplate',
-    'MailLog',
-    'MailBcc',
-]
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = '6379'
+# CELERY settings
+CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_BROKER_TRANSPORT_OPTION = {'visibility_timeout': 3600}
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+
