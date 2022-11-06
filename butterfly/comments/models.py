67d5478ad4c4f21f13f347a2b3709a449604468c
Степан Зубков <zubkovbackend@gmail.com>
2022-11-06 12:48:23 +0300

    Add product field for Comment model

--- butterfly/comments/models.py
+++ butterfly/comments/models.py
@@ -8,10 +8,12 @@ from django.db.models import (
 )
 
 from butterfly.users.models import User
+from butterfly.products.models import Product
 
 
 class Comment(Model):
     created_at = DateTimeField(auto_now_add=True)
     user = ForeignKey(User, on_delete=CASCADE)
+    product = ForeignKey(Product, on_delete=CASCADE)
     text = TextField(null=False)
     rating = PositiveSmallIntegerField(null=True)
